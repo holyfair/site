ec87858e75e2f1140351c9c77a45473ab9b208e4
kostenk0 <tnellsu@gmail.com>
2020-06-05 19:47:52 +0300

    add history api

--- src/Components/History.js
+++ src/Components/History.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import HistoryAPI from '../api'
+import { Card } from 'react-bootstrap'
+import './css/History.css'
 
 const History = () => (
-    <div>
-        <h1>Історія</h1>
+    <div className='history-main'>
+        <ul>
+            {HistoryAPI.all().map(p =>
+                (
+                    <div className='history-div'>
+                    <Card bg ='light'>
+                        <Card.Body>{p.name}</Card.Body>
+                    </Card>
+                    </div>
+                ))}
+        </ul>
     </div>
 );
 export default History;
