cca2ce297bc147c29c1d07001d31ca9823172ceb
kostenk0 <tnellsu@gmail.com>
2020-06-16 20:27:53 +0300

    add input forms

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './css/Main.css'
-import { Nav, Tab, Col, Row } from 'react-bootstrap'
+import { Nav, Tab, Col, Row, InputGroup, FormControl, Button} from 'react-bootstrap'
 import Header from './Header'
 
 export default class Main extends Component {
@@ -8,54 +8,173 @@ export default class Main extends Component {
         return (<div>
             <Header />
             <div className='main-div'>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row className="noMargin">
-                    <Col class='p-0'>
-                        <Nav variant="pills" className="flex-column" >
-                            <Nav.Item>
-                                <Nav.Link eventKey="first">Книга</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="second">Електронне джерело</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="third">Частина книги</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="fourth">Дисертація</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="fifth">Автореферат дисертації</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link eventKey="sixth">Періодичне видання</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Col>
-                    <Col>
-                        <Tab.Content >
-                            <Tab.Pane eventKey="first">
-                                <p>1</p>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                                <h2>2</h2>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="third">
-                                <h1>3</h1>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="fourth">
-                                <h1>4</h1>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="fifth">
-                                <h1>5</h1>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="sixth">
-                                <h1>6</h1>
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Col>
-                </Row>
-            </Tab.Container>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Row className="noMargin">
+                        <Col sm={2.5}>
+                            <Nav variant="pills" className="flex-column" >
+                                <Nav.Item>
+                                    <Nav.Link eventKey="first">Книга</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="second">Електронне джерело</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="third">Частина книги</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fourth">Дисертація</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="fifth">Автореферат дисертації</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="sixth">Періодичне видання</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                        <Col md='auto'>
+                            <Tab.Content >
+                                <Tab.Pane eventKey="first">
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text>ПІБ автора</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl />
+                                        <FormControl />
+                                        <FormControl />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Назва роботи</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Місто видання</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Видавництво</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Рік видання</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Кількість сторінок</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Видання</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Серія</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Номер періодичного випуску</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <Button variant="primary" size="lg" active>
+                                        Оформити
+                                    </Button>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text>ПІБ автора</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl />
+                                        <FormControl />
+                                        <FormControl />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Посилання на джерело</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Рік видання</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <InputGroup className="mb-3">
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroup-sizing-default">Видання</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <FormControl
+                                            aria-label="Default"
+                                            aria-describedby="inputGroup-sizing-default"
+                                        />
+                                    </InputGroup>
+                                    <Button variant="primary" size="lg" active>
+                                        Оформити
+                                    </Button>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <h1>Частина книги</h1>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fourth">
+                                    <h1>Дисертація</h1>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="fifth">
+                                    <h1>Автореферат дисертації</h1>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="sixth">
+                                    <h1>Періодичне видання</h1>
+                                </Tab.Pane>
+                            </Tab.Content>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </div>
         </div>
         );
